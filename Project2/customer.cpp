b9500620ac488e5fd2788f4218ac7f61cac230ab
mboonvi <mboonvi@JU56200DF>
2025-11-03 12:04:43 -0500

    added fx in cust cpp

--- Project2/customer.cpp
+++ Project2/customer.cpp
@@ -14,21 +14,55 @@ Customer::Customer(string _fname, string _lname, string _address, string _phone,
 	lname = _lname;
 	address = _address;
 	phone = _phone;
+	email = _email;
 
 }
 
-void setFirstName(string _fname);
-void setLastName(string _lname);
+ void setFirstName(string _fname); {
+	fname = _fname;
+}
+ void setLastName(string _lname); {
+	 lname = _lname;
+}
 void setAddress(string _address);
+{
+	address = _address;
+}
 void setPhone(string _phone);
-void setEmail(string _email);
+{
+	phone = _phone;
+}
+void setEmail(string _email); {
+	email = _email;
+}
 
-void setAll(string _fname, string _lname, string _address, string _phone, string _email);
+void setAll(string _fname, string _lname, string _address, string _phone, string _email) {
+	fname = _fname;
+	lname = _lname;
+	address = _address;
+	phone = _phone;
+	email = _email;
+}
 
 string getFirstName();
-string getLastName();
-string getAddress();
-string getPhone();
-string getEmail();
+{ return fname; }
+string getLastName(); {
+	return lname;
+}
+string getAddress(); {
+	return address;
+}
+string getPhone(); {
+	return phone;
+}
+string getEmail(); {
+	return email;
+}
 
-void printInfo();
+void printInfo() {
+	cout << "first name: " << fname << endl;
+	cout << "last name: " << lname << endl;
+	cout << "address: " << address << endl;
+	cout << "phone: " << phone << endl;
+	cout << "email: " << email << endl;
+}
