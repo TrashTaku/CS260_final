78a19d1bc53aeb8567c67b84ac3e238efad6ca94
TrashTaku <78836434+TrashTaku@users.noreply.github.com>
2025-11-05 10:26:44 -0500

    added some for checkingacc.h

--- Project2/checkingacc.h
+++ Project2/checkingacc.h
@@ -1,1 +1,19 @@
 #pragma once
+#ifndef CHECKINGACC_H
+#define CHECKINGACC_H
+#include "account.h"
+class CheckingAcc : public Account {
+private:
+	double overdraftlimit;
+public:
+	CheckingAcc();
+	CheckingAcc(int _id, double _balance, int _withdrawalcounter,
+		int _depositcounter,
+		Customer* _accountcustomer, double _overdraftlimit):
+		Account(_id, _balance, _withdrawalcounter, _depositcounter) ;
+
+
+
+
+};
+#endif // !CHECKINGACC_H
